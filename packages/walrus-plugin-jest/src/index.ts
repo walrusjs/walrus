7dd713d6671731820d9310990a6574301cf331d3
wangxingkang <wangxingkang@sensoro.com>
2019-09-27 02:44:47 +0800

    feat(walrus-plugin-jest): 代码优化

--- packages/walrus-plugin-jest/src/index.ts
+++ packages/walrus-plugin-jest/src/index.ts
@@ -1,5 +1,5 @@
 import { IApi } from '@walrus/types';
-import * as jest from 'jest';
+import { run } from 'jest';
 import resolve from './utils/resolve';
 import createJestConfig from './jest-config/defaultConfig';
 
@@ -7,7 +7,7 @@ const rootDir = process.cwd();
 const debug = require('debug')('walrus-plugin-jest');
 
 export default function(api: IApi) {
-  api.registerCommand('test:unit', {
+  api.registerCommand('test', {
     description: 'run unit tests with jest',
     usage: 'walrus test:unit [options] <regexForTestFiles>',
     options: {
@@ -18,8 +18,7 @@ export default function(api: IApi) {
       `See https://facebook.github.io/jest/docs/en/cli.html for more details.`
   },(args, rawArgv) => {
     rawArgv.push('--config', JSON.stringify(createJestConfig(resolve, rootDir)));
-    rawArgv.push('--watch');
-    jest.run(rawArgv)
+    run(rawArgv)
       .then((result) => {
         debug(result);
       })
--- packages/walrus-plugin-jest/src/jest-config/defaultConfig.ts
+++ packages/walrus-plugin-jest/src/jest-config/defaultConfig.ts
@@ -1,32 +1,35 @@
 const config = (resolve, rootDir) => {
   const defaultConfig = {
     rootDir: '',
-    roots: ['<rootDir>/src'],
-    setupFiles: [resolve('setupJest.js')],
-    browser: true,
-
-    testPathIgnorePatterns: ['<rootDir>/node_modules/'],
+    setupFiles: [
+      resolve('shim.js'),
+      resolve('setupJest.js')
+    ],
+    resolver: require.resolve('jest-pnp-resolver'),
 
     setupFilesAfterEnv: [resolve('setupFilesAfterEnv.js')],
     testEnvironment: 'jsdom',
 
     transform: {
-      '^.+\\.(t|j)sx$': resolve('transformers/babelTransform.js'),
+      '\\.(t|j)sx?$': resolve('transformers/babelTransform.js'),
       '^.+\\.css$': resolve('transformers/cssTransform.js'),
       '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': resolve('transformers/fileTransform.js'),
     },
     transformIgnorePatterns: [
       '[/\\\\]node_modules[/\\\\].+\\.(js|jsx|ts|tsx)$',
-      '^.+\\.module\\.(css|sass|scss)$',
+      '^.+\\.module\\.(css|sass|scss|less)$',
     ],
+    moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx', 'json'],
     moduleNameMapper: {
       '^.+\\.module\\.(css|sass|scss)$': 'identity-obj-proxy',
     },
+    testMatch: ['**/?*.(spec|test|e2e).(j|t)s?(x)'],
     watchPlugins: [
       'jest-watch-typeahead/filename',
       'jest-watch-typeahead/testname',
     ],
     verbose: true,
+    testPathIgnorePatterns: ['/node_modules/']
   };
 
   if (rootDir) {
