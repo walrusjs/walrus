2a09df50de90056a800ee25bd87f8936ae9f86c0
wangxingkang <wangxingkang@sensoro.com>
2019-09-27 01:44:32 +0800

    feat(walrus-shared-utils): add winPath isUrl compatDirname utils function

--- packages/walrus-shared-utils/src/index.ts
+++ packages/walrus-shared-utils/src/index.ts
@@ -5,10 +5,13 @@ const debug = require('debug');
 export { isLinux, isMacintosh, isWindows } from './osUtils';
 export { default as clearConsole } from './utils/clearConsole';
 export { default as Logger } from './logger';
+export { default as compatDirname } from './compatDirname';
 export { default as PluginResolution } from './pluginResolution';
 export { default as configLoader } from './configLoader';
 export { default as checkNodeVersion } from './checkNodeVersion';
 export { default as chalk } from 'chalk';
+export { default as isUrl } from './isUrl';
+export { default as winPath } from './winPath';
 export {
   semver,
   lodash,
