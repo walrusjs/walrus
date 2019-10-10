df15a208379dffa38f6094a6258c730d990016f2
wangxingkang <wangxingkang@sensoro.com>
2019-10-10 10:15:27 +0800

    feat: 代码部分提交

--- packages/walrus-plugin-prettier/src/index.ts
+++ packages/walrus-plugin-prettier/src/index.ts
@@ -5,12 +5,12 @@ import { IApi } from '@walrus/types';
 export default function(api: IApi) {
   api.registerCommand('prettier', {
     description: 'prettier source files',
-    usage: 'walrus prettier [options] [...files]',
+    usage: 'walrus prettier [opts] [filename ...]',
     options: {
       '--print-width <int>': 'print width (default: 100)'
     },
     details: 'For more options, see https://prettier.io/docs/en/options.html'
-  }, (args, opts, config) => {
-    console.log(config);
+  }, (args, opts) => {
+    console.log(opts);
   })
 }
---
+++ packages/walrus-plugin-prettier/src/processFiles.ts
@@ -0,0 +1,12 @@
+import { readFileSync, writeFileSync } from 'fs';
+import * as prettier from 'prettier';
+import { join } from 'path';
+
+const processFiles = (
+  directory: string,
+  files: string[]
+) => {
+
+};
+
+export default processFiles;
