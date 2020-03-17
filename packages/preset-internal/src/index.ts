90eb02a7cb9a38093549c76e693777e07063c2d8
kang <wang_xingkang@qq.com>
2020-03-17 11:08:01 +0800

    fix: 修复未导入问题

--- packages/preset-internal/src/index.ts
+++ packages/preset-internal/src/index.ts
@@ -1,6 +1,10 @@
 export default function() {
   return {
     plugins: [
+      // bundle configs
+      require.resolve('./plugins/features/plugins'),
+      require.resolve('./plugins/features/presets'),
+
       // commands
       require.resolve('./plugins/commands/help'),
       require.resolve('./plugins/commands/version'),
